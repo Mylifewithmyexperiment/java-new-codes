11066decc016b9d5b51fb0f3d5f01c1a5a703095
SHASHI JAISWAL <shashi.jaiswal17@gmail.com>
2019-06-26 15:56:41 +0530

    HashMap in java

---
+++ HAshmap.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class HAshmap {
+
+	public static void main(String[] args) {
+
+		HashMap<Integer, String> ar = new HashMap();
+		ar.put(1, "hvasd");
+		ar.put(1, "value");
+		ar.put(2, "sdv");
+		ar.put(null, null);
+		System.out.println(ar);
+
+		if (ar.containsKey(1)) {
+			System.out.println(" Key is present and we are removing that ");
+			System.out.println("new array " +ar.remove(1));
+			
+			
+		}
+	}
+
+}
